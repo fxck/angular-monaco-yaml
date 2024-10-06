52beb3827ea524ad2471b7669fac98c424175d69
Ales Rechtorik <foxxcz@gmail.com>
2024-10-07 00:31:10 +0200

    wip

--- src/webpack.config.ts
+++ src/webpack.config.ts
@@ -1,5 +1,23 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import * as webpack from 'webpack';
 
 export default (config: webpack.Configuration) => {
+  config.entry = {
+    ...(config.entry as any),
+    'yaml.worker': 'monaco-yaml/yaml.worker.js'
+  };
+
+  // Modify the output configuration for yaml.worker.js
+  const originalFilename = config.output!.filename;
+  config.output!.filename = (pathData: webpack.PathData) => {
+    if (pathData.chunk?.name === 'yaml.worker') {
+      return 'monaco-yaml/yaml.worker.js';
+    }
+    return typeof originalFilename === 'function'
+      ? originalFilename(pathData)
+      : originalFilename || '[name].js';
+  };
+
   return config;
 }
